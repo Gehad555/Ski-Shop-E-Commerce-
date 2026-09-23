431169247568c595fabba4a79928d4b099ef4504
Gehad Shalaby <119525675+Gehad555@users.noreply.github.com>
2026-09-23 16:16:35 +0300

    GenericRepository
    GenericRepository

---
+++ SkiShop/SkiShop.Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,72 @@
+﻿using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+using SkiShop.Core.Entities;
+
+namespace SkiShop.Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+       public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SkiShop/SkiShop.Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,25 @@
+﻿using SkiShop.Core.Entities;
+using System.Text.Json;
+
+namespace SkiShop.Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.Products.Any())
+            {
+                var path = Path.Combine(AppContext.BaseDirectory, "Data", "SeedData", "products.json");
+                var productsData = await File.ReadAllTextAsync(path);
+
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData, options);
+
+                if (products == null) return;
+
+                context.Products.AddRange(products);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
